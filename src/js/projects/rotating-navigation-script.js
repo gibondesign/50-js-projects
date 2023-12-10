f2ef86fab220a2f17e8ad02f8efa7ff51f3f8b23
gibondesign <gibonwroc@gmail.com>
2023-12-10 18:13:33 +0100

    added JS skript for nav animate and li dynamic styling

--- src/js/projects/rotating-navigation-script.js
+++ src/js/projects/rotating-navigation-script.js
@@ -1,6 +1,29 @@
-const ul = document.querySelector('ul');
-const liElements = ul.querySelectorAll('li');
+const ul = document.querySelector('ul')
+const liElements = ul.querySelectorAll('li')
 
 liElements.forEach((li, index) => {
-  li.style.marginLeft = `${index * 15}px`;
-});
+	li.style.transform = `translateX(-${115 + index * 30}%)`
+})
+
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.rotating-navigation--container')
+
+const showNav = () => {
+	container.classList.add('show-nav')
+	liElements.forEach((li, index) => {
+		li.style.marginLeft = `${index * 15}px`
+		li.style.transform = `translateX(0)`
+	})
+}
+const hideNav = () => {
+	container.classList.remove('show-nav')
+	liElements.forEach((li, index) => {
+		li.style.marginLeft = `0`
+		li.style.transform = `translateX(-${115 + index * 30}%)`
+	})
+}
+
+hideNav()
+open.addEventListener('click', showNav)
+close.addEventListener('click', hideNav)
