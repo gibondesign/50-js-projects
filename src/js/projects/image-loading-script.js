f1515abb72e26441930385596fb9d8e25cddd3f1
gibondesign <gibonwroc@gmail.com>
2023-12-12 23:39:42 +0100

    added js script

--- src/js/projects/image-loading-script.js
+++ src/js/projects/image-loading-script.js
@@ -1,1 +1,20 @@
-console.log('hello test');
+const loadText = document.querySelector('.loading-text')
+const loadNumber = document.querySelector('.loading-number')
+const loadBg = document.querySelector('.bg-img')
+
+let load = 0
+let int = setInterval(blurring, 30)
+let blurAmount = 20;
+let filterBlur = blurAmount/100;
+
+
+function blurring() {
+    load++
+	if (load > 99) {
+        clearInterval(int)
+	}
+	loadNumber.innerText = `${load}`
+	loadText.style.opacity = 1 - load/(100);
+    blurAmount -= filterBlur ;
+    loadBg.style.filter= 'blur('+blurAmount+'px)';
+}
