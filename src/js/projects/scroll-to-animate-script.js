d5a684987f3c9415af970794507440713bb3edbe
gibondesign <gibonwroc@gmail.com>
2023-12-16 00:13:10 +0100

    project 7 - split landing page

--- src/js/projects/scroll-to-animate-script.js
+++ src/js/projects/scroll-to-animate-script.js
@@ -1,18 +1,15 @@
 const boxes = document.querySelectorAll('.box')
-const windowPortion = 0.8;
-let triggerBottom = window.innerHeight * windowPortion;
+const windowPortion = 0.8
+let triggerBottom = window.innerHeight * windowPortion
 const checkBoxes = () => {
-    console.log(window.innerHeight);
-    boxes.forEach(box =>{
-
-        const boxTop = box.getBoundingClientRect().top
-        if (boxTop< triggerBottom) {
-            box.classList.add('show')  
-        } else {
-            box.classList.remove('show')  
-            
-        }
-    })
+	boxes.forEach(box => {
+		const boxTop = box.getBoundingClientRect().top
+		if (boxTop < triggerBottom) {
+			box.classList.add('show')
+		} else {
+			box.classList.remove('show')
+		}
+	})
 }
 
 checkBoxes()
