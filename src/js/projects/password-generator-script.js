48fac072c449e65e6ffc13d1ffb99c6332310749
Gib-ON Design <gibonwroc@gmail.com>
2024-02-04 13:37:50 +0100

    project 31 - password generator 1 step

--- src/js/projects/password-generator-script.js
+++ src/js/projects/password-generator-script.js
@@ -1,69 +1,41 @@
-const bodyElement = document.body;
-const hourEl = document.querySelector('.hour')
-const minuteEl = document.querySelector('.minute')
-const secondEl = document.querySelector('.second')
-const timeEl = document.querySelector('.time')
-const dayEl = document.querySelector('.day')
-const monthEl = document.querySelector('.month')
-const circleEl = document.querySelector('.circle')
-const toggleEl = document.querySelector('.theme-toggle')
-let i = 0
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("result");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateBtn = document.getElementById("generateBtn");
+const clipboardEl = document.getElementById("clipboard");
 
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol,
+};
 
+const getRandomLower = () => {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
 
- // set default dark mode, based on the browser's preferences
- if (
-	window.matchMedia('(prefers-color-scheme)').media !== 'not all' &&
-	window.matchMedia('(prefers-color-scheme: dark)').matches
-  ) {
-	bodyElement.classList.add('dark');
-	if (bodyElement.classList.contains('dark')) {
-	  toggleEl.innerText = 'Light mode';
-	} else {
-	  toggleEl.innerText = 'Dark mode';
-	}
-  }
+const getRandomUpper = () => {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
 
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursdat', 'Friday', 'Saturday', 'Sunday']
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const getRandomNumber = () => {
+	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+const getRandomSymbol = () => {
+	const symbols = "!@#$%&()^";
+	return symbols[Math.floor(Math.random() * symbols.length)];
+};
 
-setTime()
+const getSettings = () => {
+	const length = parseInt(lengthEl.value);
+	const hasUpper = uppercaseEl.checked
+	const hasLower = lowercaseEl.checked
+	const hasNumbers = numbersEl.checked
+	const hasSymbols = symbolsEl.checked
+};
 
-const modeSwitch = () => {
-	const body = document.querySelector('body')
-	if (body.classList.contains('dark')) {
-		body.classList.remove('dark')
-		toggleEl.innerText = 'Dark mode'
-	} else {
-		body.classList.add('dark')
-		toggleEl.innerText = 'Light mode'
-	}
-}
-
-function setTime() {
-	const time = new Date()
-	const month = time.getMonth()
-	const day = time.getDay()
-	const dayNumber = time.getDate()
-	const hours = time.getHours()
-	const minutes = time.getMinutes()
-	const seconds = time.getSeconds()
-	
-
-	if (seconds == 0) {
-		i = i + 360
-	}
-
-
-	hourEl.style.transform = `translate(-50%, -100%) rotate(${hours * 30}deg)`
-	minuteEl.style.transform = `translate(-50%, -100%) rotate(${minutes * 6}deg)`
-	secondEl.style.transform = `translate(-50%, -100%) rotate(${seconds * 6 + i}deg)`
-	timeEl.innerText = `${hours}:${minutes <10 ?`0${minutes}` : minutes}`
-	dayEl.innerText = `${days[day]},`
-	monthEl.innerText = `${months[month]}`
-	circleEl.innerText = dayNumber
-}
-
-setInterval(setTime, 1000)
-
-toggleEl.addEventListener('click', modeSwitch)
+generateBtn.addEventListener("click", getSettings);
